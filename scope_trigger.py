4217db98454f900d1b4ea24520d465ccc605a259
shared account on pi <pi@localhost>
2017-03-02 18:23:15 -0500

    sped up pulses somewhat

--- scope_trigger.py
+++ scope_trigger.py
@@ -2,10 +2,12 @@ import RPi.GPIO as g
 import time
 g.setmode(g.BCM)
 g.setup(21,g.OUT)
-def trigger_scope(wait=100e-6):
-    """Just quickly flashes the voltage on the scope
+def trigger_scope(wait=1e-6):
+    """Just quickly flashes the voltage on the scope on GPIO21, which can then be used as a trigger.
 
-    On 3/2/17, tested on Tektronix 2465 CTS 20 mV/div (which seems to be incorrect), A1 trigger level to 360 mV, 100 us/div"""
+    On 3/2/17, tested on Tektronix 2465 CTS 20 mV/div (which seems to be incorrect), A1 trigger level to 360 mV, 50 us/div.
+    Yields pulses about 80 us wide (though you can get much faster by eliminating the "wait" statements.
+    """
     g.output(21,True)
     time.sleep(wait)
     g.output(21,False)
@@ -13,7 +15,7 @@ def trigger_scope(wait=100e-6):
     g.output(21,True)
     time.sleep(wait)
     g.output(21,False)
-for j in range(100):
+for j in range(10000):
     trigger_scope()
-    time.sleep(0.5)
+    time.sleep(0.001)
 
